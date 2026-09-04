2bce9f0943d971559a5a6aadf7549869f1b4e173
codepriyanshu2004 <priyanshubitturajs@gmail.com>
2026-09-04 09:11:18 +0530

    Completed Course list page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import HomePage from './Pages/HomePage.jsx'
 import NotFound from './Pages/NotFound.jsx'
 import Signup from './Pages/Signup.jsx'
 import Login from './Pages/Login.jsx'
+import CourseList from './Pages/Course/CourseList.jsx'
 
 function App() {
  
@@ -19,6 +20,7 @@ function App() {
    <Routes>
       <Route path="/" element={<HomePage/>}> </Route>
       <Route path='/AboutUs' element={<Aboutus/>}> </Route>
+      <Route path='/courses' element={<CourseList/>}> </Route>
 
       <Route path='/signup' element={<Signup/>}></Route>
       <Route path='/login' element={<Login/>}> </Route>
